57b47d907a7a22283d7c4040befffc5e99df67ea
Mike Bishop <mbishop@evequefou.be>
2025-04-25 09:45:33 -0400

    Correct format

--- teslausb-www/html/cgi-bin/pairBLEkey.sh
+++ teslausb-www/html/cgi-bin/pairBLEkey.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VIN=$(grep "^${TESLA_BLE_VIN}=" /root/teslausb_setup_variables.conf | cut -d'=' -f2- | head -n 1)
+VIN=$(grep "^export TESLA_BLE_VIN=" /root/teslausb_setup_variables.conf | cut -d'=' -f2- | head -n 1)
 
 /root/bin/tesla-control -ble -vin $VIN add-key-request /root/.ble/key_public.pem owner cloud_key
 
