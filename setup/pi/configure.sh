c9357ea50195861f07be75bf36bdcffa5e018c5a
Mike Bishop <mbishop@evequefou.be>
2025-05-01 09:35:36 -0400

    Revert bluez installation entirely

--- setup/pi/configure.sh
+++ setup/pi/configure.sh
@@ -303,8 +303,6 @@ function install_tesla_ble_packages () {
   local install_path="$1"
   local binary_dir=/tmp/binarydir
 
-  apt install -y bluetooth
-
   umount "$binary_dir" &> /dev/null || true
   rm -rf "$binary_dir"
   mkdir -p "$binary_dir"
